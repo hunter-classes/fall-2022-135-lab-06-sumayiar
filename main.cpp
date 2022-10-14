ac669f600c811ab8d3b157e3d04e248afebc032f
Sumayia Rashid <sumayiarashid@Sumayia.lan>
2022-10-14 07:43:34 -0400

    didn't finish

--- main.cpp
+++ main.cpp
@@ -1,8 +1,11 @@
 #include <iostream>
 #include "funcs.h"
+#include "vigenere.cpp"
+#include "caesar.cpp"
 
 int main()
 {
+  //Task B
   std::string s;
   int shift;
 
@@ -14,5 +17,16 @@ int main()
 
   std::cout << "Ciphertext: " << encryptCaesar(s, shift) << std::endl;
 
+  //Task C
+  std:: string t;
+  int key;
+
+  std::cout << "Enter plaintext: " << std::endl;
+  getline (std::cin, t);
+
+  std::cout << "Enter keyword: " << std::endl;
+  std::cin >> key;
+
+  std::cout << "Ciphertext: " << encryptVigenere(t, key) << std::endl;
   return 0;
 }
--- vigenere.cpp
+++ vigenere.cpp
@@ -10,12 +10,20 @@ This program implementS Caesar cipher encryption based on a shift given by the u
 #include <string>
 
 std::string encryptVigenere(std::string plaintext, std::string keyword);
-
-int main(){
-
-    return 0;
-}
-
 std::string encryptVigenere(std::string plaintext, std::string keyword){
+    std::string s;
+    char c;
+    int index = 0;
+    int key;
     
+    for (int i = 0; i < plaintext.length(); i++){
+        c = plaintext[i];
+        if (c >= 'A' && c <= 'Z'){
+            key = keyword[index] - 'a';
+            index = (index + 1) % keyword.length();
+            c = 'a' + (c -'a' + key + 26) % 26;
+        }
+        s += c;
+    }
+    return s;
 }
